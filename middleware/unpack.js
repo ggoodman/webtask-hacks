dd11aef4d8fe742b46bc429e8fc812c6a177e916
Geoffrey Goodman <ggoodman@gmail.com>
2018-01-23 13:31:09 -0500

    Fix export naming for unpack middleware

--- middleware/unpack.js
+++ middleware/unpack.js
@@ -2,13 +2,15 @@
 
 const Decompress = require('decompress');
 
-module.exports = () => (req, res, next) => {
+exports.middleware = () => (req, res, next) => {
     const webtaskContext = req.webtaskContext;
     const code = Buffer.from(webtaskContext.compiler.script, 'base64');
-    const path = `/tmp/${Math.random().toString(36).slice(2,7)}`
+    const path = `/tmp/${Math.random()
+        .toString(36)
+        .slice(2, 7)}`;
 
     return Decompress(code, path).then(() => {
-        webtaskContext.compiler.script = `module.exports = require('${path}');`
+        webtaskContext.compiler.script = `module.exports = require('${path}');`;
 
         return next();
     }, next);
